c83b244c8576f30607198a5bd3f316839a9b5303
Stephan Kochauf, Campus02 <stephan.kochauf@lv.campus02.at>
2024-10-09 19:15:41 +0200

    DemoRezeptApp mit Zutaten und Rezept

--- src/DemoRezeptApp.java
+++ src/DemoRezeptApp.java
@@ -2,7 +2,24 @@ public class DemoRezeptApp {
 
     public static void main(String[] args) {
 
+        Zutat mehl = new Zutat();
+        mehl.setName("Mehl");
+        mehl.setMenge(300);
 
+        Zutat zucker = new Zutat();
+        zucker.setName("Wiener Zucker");
+        zucker.setMenge(300);
+
+        Zutat butter = new Zutat();
+        butter.setName("Butter BIO");
+        butter.setMenge(300);
+
+        Zutat[] zutaten = new Zutat[] {mehl, zucker, butter};
+
+        Rezept butterKeks = new Rezept();
+        butterKeks.setName("Süßer Butterkeks");
+        butterKeks.setPersonen(4);
+        butterKeks.setZutaten(zutaten);
 
 
 
